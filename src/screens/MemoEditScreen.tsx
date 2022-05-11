5b5eb55373e0e53cfe27c5d52d386e6c8a95ff95
isseimiyauchi <mi1209tad@gmail.com>
2022-05-11 18:17:05 +0900

    add MemoEditScreen

---
+++ src/screens/MemoEditScreen.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import AppBar from "../components/AppBar";
+import CircleBotton from "../components/CircleButton";
+
+export default function MemoEditScreen() {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <View style={styles.inputContainer}>
+        <TextInput value='買い物リスト' multiline style={styles.input} />
+      </View>
+      <CircleBotton name="check" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    paddingHorizontal: 27,
+    paddingVertical:32,
+    flex:1,
+  },
+  input: {
+    fontSize: 16,
+    lineHeight: 24,
+    flex: 1,
+    textAlignVertical: 'top'
+  }
+});
